52160e815b219d34075a7cd42b02e95e04f3fc58
Daniyar Bazakov <bazakovd@gmail.com>
2024-06-21 10:27:31 -0400

    impliment generateRandomString function

--- express_server.js
+++ express_server.js
@@ -6,7 +6,15 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-function generateRandomString() {}
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
